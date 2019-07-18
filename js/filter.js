2dcbb0fc7cd45d949e53e69d72b4a2ebf3db41e2
Ulyana <ulyanakapeshina@gmail.com>
2019-07-18 17:14:49 +0300

    исправлены ошибки предзащиты

--- js/filter.js
+++ js/filter.js
@@ -2,12 +2,13 @@
 
 (function () {
   var ANY_TYPE = 'any';
+  var MIN_PRICE = 10000;
+  var MAX_PRICE = 50000;
   var housingTypeSelect = document.querySelector('#housing-type');
   var housingPriceSelect = document.querySelector('#housing-price');
   var housingRoomsSelect = document.querySelector('#housing-rooms');
   var housingGuestsSelect = document.querySelector('#housing-guests');
-  var MIN_PRICE = 10000;
-  var MAX_PRICE = 50000;
+
 
   var getTypePrice = function (price) {
     if (price > MAX_PRICE) {
@@ -28,45 +29,34 @@
     var validateNull = function (element) {
       return typeof element !== 'undefined' && element !== null;
     };
-    data = data.filter(function (ad) {
-      return validateNull(ad) && validateNull(ad.offer);
-    });
+    var filterTypeOfHousing = function (card) {
+      return checkedTypeOFHousing === ANY_TYPE || card.offer.type === checkedTypeOFHousing;
+    };
 
-    if (checkedTypeOFHousing !== ANY_TYPE) {
-      data = data.filter(function (ad) {
-        return ad.offer.type === checkedTypeOFHousing;
-      });
-    }
-
-    if (checkedRooms !== ANY_TYPE) {
-
-      data = data.filter(function (ad) {
-        return ad.offer.rooms === parseInt(housingRoomsSelect.value, 10);
-      });
-    }
-    if (checkedPrice !== ANY_TYPE) {
-      data = data.filter(function (ad) {
-        return checkedPrice === getTypePrice(ad.offer.price);
-      });
-    }
-    if (checkedGuests !== ANY_TYPE) {
-      data = data.filter(function (ad) {
-
-        return ad.offer.guests === parseInt(checkedGuests, 10);
-      });
-    }
+    var filterRooms = function (card) {
+      return checkedRooms === ANY_TYPE || card.offer.rooms === parseInt(housingRoomsSelect.value, 10);
+    };
+    var filterPrice = function (card) {
+      return checkedPrice === ANY_TYPE || checkedPrice === getTypePrice(card.offer.price);
+    };
+    var filterGuests = function (card) {
+      return checkedGuests === ANY_TYPE || card.offer.guests === parseInt(checkedGuests, 10);
+    };
     var featureFilter = document.querySelectorAll('input[name=features]:checked');
     var featuresValues = Array.from(featureFilter).map(function (feature) {
       return feature.value;
     });
 
-    if (featuresValues.length > 0) {
-      data = data.filter(function (ad) {
-        return featuresValues.every(function (feature) {
-          return ad.offer.features.includes(feature);
-        });
+    var filterFeature = function (card) {
+      return featuresValues.length === 0 || featuresValues.every(function (feature) {
+        return card.offer.features.includes(feature);
       });
-    }
+    };
+
+    data = data.filter(function (ad) {
+      return validateNull(ad) && validateNull(ad.offer) && filterTypeOfHousing(ad) && filterRooms(ad) && filterPrice(ad) && filterGuests(ad) && filterFeature(ad);
+
+    });
     return data;
   };
 
