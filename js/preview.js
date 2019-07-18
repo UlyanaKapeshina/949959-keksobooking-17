423fc95e04cfd552caa3a9a7c39c4abc3a41f441
Ulyana <ulyanakapeshina@gmail.com>
2019-07-18 17:16:59 +0300

    добавлен модуль для предпросмотра фотографий

---
+++ js/preview.js
@@ -0,0 +1,54 @@
+'use strict';
+(function () {
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+  // создание элемента разметки для загрузки фотографии
+
+  var renderImg = function (container, div) {
+    var image = document.createElement('img');
+    image.alt = 'Фотография жилья';
+    image.width = '70';
+    image.height = '70';
+    image.style = 'border-radius: 5px';
+    var newDiv = div.cloneNode();
+    newDiv.draggable = true;
+    newDiv.style = 'cursor: move; user-select: none';
+    newDiv.appendChild(image);
+    container.appendChild(newDiv);
+    return image;
+  };
+
+  // загрузка фоторгафии если формат соответствует
+
+  var image = function (file, preview) {
+    if (checkFileType(file)) {
+      readFile(preview, file);
+    }
+  };
+
+  // чтение url файла и запись адреса изображения
+
+  var readFile = function (preview, file) {
+    var reader = new FileReader();
+
+    reader.addEventListener('load', function () {
+      preview.src = reader.result;
+    });
+    reader.readAsDataURL(file);
+  };
+
+  // проверка типа загружаемого файла
+
+  var checkFileType = function (file) {
+    var fileName = file.name.toLowerCase();
+
+    return FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+  };
+
+  window.preview = {
+    image: image,
+    render: renderImg
+  };
+})();
