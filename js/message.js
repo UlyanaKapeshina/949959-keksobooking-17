ad5551ac509d7c5ff7a26c7823aaac171f5a20b1
Ulyana <ulyanakapeshina@gmail.com>
2019-07-18 17:16:12 +0300

    добавлен модуль создающий и удаляющий сообщения об ошибке при загрузке и отправке данных

---
+++ js/message.js
@@ -0,0 +1,56 @@
+'use strict';
+(function () {
+
+
+  // закрытие сообщения по нажатию ESC
+
+  var onEscPress = function (evt) {
+    window.util.isEscEvent(evt, onErrorCloseClick);
+  };
+
+  // закрытие сообщения об ошибке
+
+  var onErrorCloseClick = function () {
+    var errorButton = document.querySelector('.error__button');
+    document.querySelector('.error').remove();
+    document.removeEventListener('keydown', onEscPress);
+    errorButton.removeEventListener('click', onErrorCloseClick);
+    document.removeEventListener('click', onErrorCloseClick);
+  };
+
+  // создание сообщения об ошибке если ошибка в присланных с сервера данных об объявлениях
+
+  var onErrorData = function () {
+    onError();
+    window.map.deactivate();
+    window.form.getDisabled();
+  };
+
+  // создание сообщения об ошибке если ошибка возникла при отправлении формы
+
+  var onErrorForm = function () {
+    onError();
+    window.form.setAddress();
+  };
+
+
+  var onError = function (message) {
+    var errorTemplate = document.querySelector('#error').content.querySelector('.error');
+    var main = document.querySelector('main');
+    var error = errorTemplate.cloneNode(true);
+    error.querySelector('.error__message').textContent = message;
+    main.appendChild(error);
+    var errorButton = document.querySelector('.error__button');
+
+    errorButton.addEventListener('click', onErrorCloseClick);
+    document.addEventListener('click', onErrorCloseClick);
+    document.addEventListener('keydown', onEscPress);
+
+  };
+
+  window.message = {
+    onErrorData: onErrorData,
+    onErrorForm: onErrorForm
+  };
+
+})();
