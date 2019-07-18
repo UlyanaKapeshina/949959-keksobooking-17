0a801244f87ebb817c54b1c0671c0b28b67e690c
Ulyana <ulyanakapeshina@gmail.com>
2019-07-18 17:19:02 +0300

    добавлены константы

--- js/pin.js
+++ js/pin.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var PIN_HEIGHT = 70;
+  var PIN_WIDTH = 50;
   var similarPinTemplate = document.querySelector('#pin').content.querySelector('.map__pin');
 
   // создать пин
@@ -8,8 +10,8 @@
   var renderPin = function (ad) {
     var pin = similarPinTemplate.cloneNode(true);
     pin.classList.add('pin');
-    pin.style.left = (ad.location.x - window.constants.PIN_WIDTH / 2) + 'px';
-    pin.style.top = (ad.location.y - window.constants.PIN_HEIGHT) + 'px';
+    pin.style.left = (ad.location.x - PIN_WIDTH / 2) + 'px';
+    pin.style.top = (ad.location.y - PIN_HEIGHT) + 'px';
     pin.querySelector('img').src = ad.author.avatar;
     pin.querySelector('img').alt = ad.offer.type;
     return pin;
